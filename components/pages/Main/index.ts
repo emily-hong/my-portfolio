89b8fd39237d34ecc8cc5c22fbc197338c42dd45
emily-hong <72483407+emily-hong@users.noreply.github.com>
2024-11-29 19:09:32 +0900

    Delete components directory

--- components/pages/Main/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as FirstSection } from "./FirstSection.component";
-export { default as SecondSection } from "./SecondSection.component";
-export { default as FourthSection } from "./FourthSection.component";
-export { default as ThirdSection } from "./ThirdSection.component";
