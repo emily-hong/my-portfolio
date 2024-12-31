34e70e02b616526947e7f9c75c18d7a8f16f63e9
홍현정 <emilyhong4659@gmail.com>
2024-12-31 22:34:32 +0900

    fix: vercel 이슈, i18n.ts 위치 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/global.css"; // 글로벌 CSS 경로
 import { AppProps } from "next/app";
-import "./i18n";
+import "../i18n";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
